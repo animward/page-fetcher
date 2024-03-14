0eae88679553a8f45e396eb2d881f93070fb94a0
EgoExperiments <egoexperiments@gmail.com>
2024-03-13 18:36:38 -0700

    Done everything and all stretch, youtube is in indexhtml

--- fetcher.js
+++ fetcher.js
@@ -4,11 +4,38 @@ const request = require('request');
 // use Node's fs (file system)
 const fs = require('fs');
 
-// make HTTP request
-request(url, error, response, body); {
-    if (error) {
-        console.error("Error occurred while making HTTP request", error);
-        return;
-    }
+// function to download URL to local directory
 
-}
+const args = process.argv.slice(2);
+const url = args[0];
+const filePath = args[1];
+
+const downloadResource = (url, filePath) => {
+    // make HTTP request
+    request(url, (error, response, body) => {
+        if (error) {
+            console.error("Error occurred while making HTTP request", error);
+            return;
+        }
+
+        if (response.statusCode !== 200) {
+            console.error(`Failed to download resource. Status code: ${response.statusCode}`);
+            return
+        }
+
+        fs.writeFile(filePath, body, (err) => {
+            if (err) {
+                console.error("Error occurred while writing to file:", err);
+                return;
+            }
+
+            const fileSize = body.length;
+            console.log(`Downloaded and saved ${fileSize}bytes to ${filePath}`);
+        })
+    })
+};
+    
+
+
+
+downloadResource(url, filePath);
